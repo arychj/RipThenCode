2a98d2acebfd779e65105426af6dc0678466c6ad
Erik J. Olson <git@arych.com>
2015-02-02 10:35:58 -0500

    Updated variable naming and calling conventions

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Scripts to rip and encode blurays
 
 Usage
 =====
-1. Run ```./rip.sh``` to rip the bluray (rips to *.rip to be picked up by encode.sh)
+1. Run ```./rip.sh $processingdir``` to rip the bluray (rips to *.rip to be picked up by encode.sh)
 2. Run ```./encode.sh $processingdir $outputdir``` to encode the ripped titles (looks for *.rip and encodes to *.mkv)
--- rip.sh
+++ rip.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+processingdir=$1	
+
 sourcedrive="/dev/sr0"
-outputdir="/tmp/rips"
 extension="rip"
 minlength=1800
 notificationCommand=""
@@ -13,7 +14,7 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [[ `ls -l /dev/disk/by-label/ | grep sr0 | wc -l` == 1 ]]; then
 	dtstart=`date`
 
-	mkdir -p $outputdir
+	mkdir -p $processingdir
 
 	title=$(blkid -o value -s LABEL $sourcedrive)
 	title=${title// /_}
@@ -22,13 +23,13 @@ if [[ `ls -l /dev/disk/by-label/ | grep sr0 | wc -l` == 1 ]]; then
 		$notificationCommand "$title has started ripping"
 	fi
 
-	makemkvcon mkv disc:0 all $outputdir --decrypt --minlength=$minlength
+	makemkvcon mkv disc:0 all $processingdir --decrypt --minlength=$minlength
 
 	makemkvcon -r info disc:0 --minlength=$minlength > $infofile
-	$dir/order.py $outputdir $infofile
+	$dir/order.py $processingdir $infofile
 	rm $infofile
 
-	for file in $(find $outputdir -name '*.mkv'); do
+	for file in $(find $processingdir -name '*.mkv'); do
 		destfile="${file%.mkv}".$extension
 		mv $file $destfile
 		chmod 777 $destfile
